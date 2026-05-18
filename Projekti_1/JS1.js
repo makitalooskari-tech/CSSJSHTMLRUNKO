928a54b538345c9aa57916a3705cfbf67539455f
makitalooskari-tech <makitalooskari@gmail.com>
2026-05-18 05:11:19 +0300

    Add files via upload

---
+++ Projekti_1/JS1.js
@@ -0,0 +1,15 @@
+
+
+/*
+
+
+function muunna() {
+  let teksti = document.getElementById("input").value;
+  alert(teksti);
+}
+
+function testaa() {
+  alert("Testinappi toimii!");
+}
+
+*/
---
+++ Projekti_1/JS3.js
@@ -0,0 +1,19 @@
+
+/*
+
+
+function paivitaZoom() {
+  const zoom = window.visualViewport ? window.visualViewport.scale : 1;
+  document.documentElement.style.setProperty("--zoom", zoom);
+}
+
+paivitaZoom();
+
+if (window.visualViewport) {
+  window.visualViewport.addEventListener("resize", paivitaZoom);
+}
+window.addEventListener("resize", paivitaZoom);
+
+
+
+*/
